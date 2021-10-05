0ac23d77d0b4086edec8377ef6d857d1681b0c29
Tetiana <latysh.tetiana@airslate.com>
2021-10-05 09:32:44 +0300

    car

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Car from './Car/Car'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cars: [
+      {name: 'Audi', year: '2016'},
+      {name: 'Ford', year: '2019'},
+      {name: 'Mazda', year: '2015'}
+    ],
+    pageTitle: 'This is CARS!',
+    showCars: false
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index];
+    car.name = name;
+    const cars = [...this.state.cars];
+    cars[index] = car;
+    this.setState({cars});
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat();
+    cars.splice(index, 1);
+    this.setState({cars});
+  }
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars : !this.state.showCars
+    })
+  }
+
+  render() {
+    const divStyle = {
+      textAlign : 'center'
+    }
+
+    let cars = null;
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+            <Car
+                key={index}
+                name={car.name}
+                year={car.year}
+                onDelete={this.deleteHandler.bind(this, index)}
+                onChangeName={(event) => this.onChangeName(event.target.value, index)}
+            />
+        )
+      })
+    }
+
+    return (
+      <div style={divStyle} >
+        <h1>{this.state.pageTitle}</h1>
+
+        <button onClick={this.toggleCarsHandler}>Toggle cars</button>
+
+        <div style={{
+          width: '400px',
+          margin: 'auto',
+          paddingTop: 20
+        }}>
+
+          { cars }
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Car.css';
+
+// function Car() {
+//     return (
+//         <div>This is car component</div>
+//     )
+// }
+
+// const Car = () => {
+//     return (
+//         <div>This is car component</div>
+//     )
+// }
+
+// const Car = () => (
+//     <div>This is car component</div>
+// );
+
+// export default Car;
+
+// eslint-disable-next-line
+export default (props) => {
+    const inputClasses = ['input'];
+    if (props.name != '') {
+        inputClasses.push('green');
+    } else {
+        inputClasses.push('red');
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold');
+    }
+
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+    }
+
+    return (
+        <div className="Car" style={style}>
+            <h3>Car name: {props.name}</h3>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input
+                type="text"
+                onChange={props.onChangeName}
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    );
+}
